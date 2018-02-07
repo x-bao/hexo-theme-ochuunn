ac2158b2c2c18c12bc4e53287097f28678e2fa76
bao <baoyeyene@gmail.com>
2018-02-07 12:08:50 +0800

    1. 首页导航 2. 响应式处理

--- source/js/indexNav.js
+++ source/js/indexNav.js
@@ -1,8 +1,10 @@
 const indexNavItems = $('.index-nav__list span');
 const links = $('.index-nav__list a');
 
-let index = 4;
+let index = 5;
+let order = 0
 const timer = setInterval(() => {
-	const i = index % 4;
-	$(indexNavItems[i]).css('zIndex', index++);
+	const item = $(indexNavItems[index % 5])
+	item.css('zIndex', index++);
+	// item.css('order', order--);
 }, 1000);

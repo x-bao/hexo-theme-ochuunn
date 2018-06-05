fce22e7f0456fdf48f9cef92389c3c362ca0f78c
bao <baoyeyene@gmail.com>
2018-06-05 22:33:29 +0800

    update

--- source/js/indexNav.js
+++ source/js/indexNav.js
@@ -1,5 +1,20 @@
 var indexNavItems = $('.index-nav__list span');
 var links = $('.index-nav__list a');
+var indexNav = $('.index-nav__list');
+
+window.onload = function () {
+    indexNav.animate({}, 0, open).animate({}, 200, close);
+}
+
+function open () {
+    indexNav.addClass('open');
+}
+
+function close () {
+    setTimeout(function () {
+        indexNav.removeClass('open');
+    }, 720);
+}
 
 var index = 5;
 var timer = setInterval(function () {
